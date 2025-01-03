7ed0c69cf9bcfd04b4516def24591f2c5746f38e
yuntianOS <365019475@qq.com>
2025-01-03 10:46:23 +0800

    工艺路线 试验记录 暂存

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,12 @@ import type { UmoEditorOptions } from '@/types'
 import App from './app.vue'
 import { useUmoEditor } from './components'
 //我想引用mzyc-vue-components-zzw项目里面的 /dist/lib/mzyc-lib.es.js
-import MzycLib from '../../mzyc-web/libs/mzyc-vue-components/outDist/lib/mzyc-lib.es.js'
-import '../../mzyc-web/libs/mzyc-vue-components/outDist/lib/mzyc-vue-components.css'
+// import MzycLib from '../../mzyc-web/libs/mzyc-vue-components/outDist/lib/mzyc-lib.es.js'
+// import '../../mzyc-web/libs/mzyc-vue-components/outDist/lib/mzyc-vue-components.css'
+
+//快捷編譯
+import MzycLib from '../../mzyc-vue-components-zzw/dist/lib/mzyc-lib.es.js'
+import '../../mzyc-vue-components-zzw/dist/lib/mzyc-vue-components.css'
 
 const app = createApp(App)
 // // 引入组件库的少量全局样式变量
